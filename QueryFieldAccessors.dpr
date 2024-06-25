d5edb94b9a2546fc262d255d4460b348f48c23f9
Jim McKeeth <jim@mckeeth.org>
2024-06-25 19:48:19 +0200

    Added theme

--- QueryFieldAccessors.dpr
+++ QueryFieldAccessors.dpr
@@ -2,13 +2,16 @@ program QueryFieldAccessors;
 
 uses
   Vcl.Forms,
-  QueryFieldAccessorsMain in 'QueryFieldAccessorsMain.pas' {Form1};
+  QueryFieldAccessorsMain in 'QueryFieldAccessorsMain.pas' {Form1},
+  Vcl.Themes,
+  Vcl.Styles;
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
+  TStyleManager.TrySetStyle('Windows11 Impressive Dark');
   Application.CreateForm(TForm1, Form1);
   Application.Run;
 end.
